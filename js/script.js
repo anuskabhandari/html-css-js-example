7d04d8e4b358450aaff7fa4bfa318c1e2f358439
anuskabhandari <bhandarirekha652@gmail.com>
2026-03-09 11:05:55 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("btn").addEventListener("click", function() {
+        document.getElementById("title").innerHTML = "Text has been changed using JavaScript!";
+    });
+
+});
